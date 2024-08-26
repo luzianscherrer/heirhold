cf2d586a0b81db5c436e18740d09c0642d1b491f
Luzian Scherrer <luzian@scherrer.ch>
2024-08-26 21:08:55 +0200

    cleanup

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -3,8 +3,8 @@ import { Container, Row } from "react-bootstrap";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider, useAccount } from "wagmi";
 import { config } from "./config";
-import { WalletOptions } from "./wallet-options";
-import { Account } from "./account";
+import { WalletOptions } from "./WalletOptions";
+import { Account } from "./Account";
 import Logo from "./logo.svg";
 
 const queryClient = new QueryClient();
